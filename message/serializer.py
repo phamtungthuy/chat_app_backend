6fb1f8348f8784c4d35f9b0faff7abd767d185dc
phamtungthuy <94853261+phamtungthuy@users.noreply.github.com>
2024-05-19 20:11:40 +0700

    Preprocess reply message before returning data to fe

--- message/serializer.py
+++ message/serializer.py
@@ -12,6 +12,22 @@ class MessageSerializer(serializers.ModelSerializer):
         data = super(MessageSerializer, self).to_representation(instance)
         memberSerializer = channel.serializer.MemberSerializer(instance.member, many=False)
         data['member'] = memberSerializer.data
+        data['channel'] = channel.serializer.ChannelSerializer(instance.channel, many=False).data
+        if data["reply"]:
+            reply = data["reply"]
+            message = Message.objects.get(pk=reply)
+            data["reply"] = {
+                "_id": reply,
+                "text": message.content
+            }
+            if message.image:
+                data["reply"]["image"] = message.image
+            if message.location:
+                locationArr = message.location.split(" ")
+                data["reply"]["location"] = {
+                    "latitude": float(locationArr[0]),
+                    "longitude": float(locationArr[1])
+                }
         return data
 
 
